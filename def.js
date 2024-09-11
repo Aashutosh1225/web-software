ba79a514f81dabd5e6d08c3bec427a81d6bf6680
Aashutosh Adhikari <023neb712@sxc.edu.np>
2024-09-11 14:17:10 +0545

    Create def.js

---
+++ def.js
@@ -0,0 +1,7 @@
+function showDialog(dialogId) {
+            document.getElementById(dialogId).style.display = 'block';
+        }
+
+        function hideDialog(dialogId) {
+            document.getElementById(dialogId).style.display = 'none';
+        }
